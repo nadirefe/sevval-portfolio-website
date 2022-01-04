903f0eb5c66c458b587d2fb70eaecd6472cad964
Efe <efenadir@sabanciuniv.edu>
2022-01-04 19:11:10 +0300

    remove comment

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,7 @@ import Container from '../components/Container';
 const Home = () => {
   return (
     <div>
-      <Container isHome={true} title='Sevval Tufan'>
-        {/* <div className="bg-idcol w-full h-screen bg-cover bg-center bg-no-repeat bg-top" /> */}
-        {/* <div className='relative'>
-          <img className='absolute collage' src='/images/idcollage.jpeg' />
-          <img className='absolute purple-cloud-bottom' src='/images/purple-cloud-bottom.jpeg' />
-          <img className='absolute purple-cloud-up' src='/images/purple-cloud-up.jpeg' />
-          <img className='absolute pink-cloud-bottom' src='/images/pink-cloud-bottom.jpeg' />
-          <img className='absolute pink-cloud-up' src='/images/pink-cloud-up.jpeg' />
-          <img className='absolute green-cloud' src='/images/green-cloud.jpeg' />
-        </div> */}
-      </Container>
+      <Container isHome={true} title='Sevval Tufan'></Container>
       <div className='relative'>
         <div className='purple-cloud-bottom absolute inset-0 bg-no-repeat bg-contain w-full h-screen bg-center'></div>
         <div className='pink-cloud-up absolute inset-0 bg-no-repeat bg-contain w-full h-screen bg-center'></div>
